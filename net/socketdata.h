c502e639e5907ae4c1bb679ecab215d57392a560
zero kuang <izerocrazy@gmail.com>
2014-11-19 20:55:14 +0800

    同步代码 1、add socketdata

---
+++ net/socketdata.h
@@ -0,0 +1,18 @@
+#ifndef __SOCKET_DATA_H__
+#define __SOCKET_DATA_H__
+
+#include "../base/include/commondefine.h"
+
+// 双全工 handle
+// 记录本地的 ip port
+// 记录远程的 ip port
+class KSocketData {
+	private:
+		unsigned int m_hSocketHandle;
+		DWORD m_nLocalIP;
+		int m_nLocalPort;
+		DWORD m_nRemoteIP;
+		int m_nRemotePort
+}
+
+#endif
